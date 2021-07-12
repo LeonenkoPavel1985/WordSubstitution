a049b33a68c3e0909e30651803b2fc9eb5e5f3e4
LeonenkoPavel1985 <77398240+LeonenkoPavel1985@users.noreply.github.com>
2021-07-12 14:34:00 +0700

    Add files via upload

---
+++ reverseTest.cs
@@ -0,0 +1,18 @@
+}
+} 
+
+}
+
+
+{	
+)(tset diov cilbup
+	
+} ;tes ;teg { dedniB loob cilbup 
+} ;tes ;teg { epyTtinU gnirts cilbup 
+} ;tes ;teg { edoC gnirts cilbup 
+} ;tes ;teg { rebmuN gnirts cilbup 
+} ;tes ;teg { dI gnol cilbup 
+{ 
+ledoMdniBtinU	 ssalc cilbup 
+{
+rekraPtelaV.sledoM.ssenisuB.ssenisuBataD.CMWK ecapseman
---
+++ test.cs
@@ -0,0 +1,18 @@
+namespace KWMC.DataBusiness.Business.Models.ValetParker
+{
+    public class 						UnitBindModel
+    {
+        public long Id     { get; set; }
+        public string    Number { get; set; }
+        public string Code { get; set; }
+        public string UnitType { get; set; }
+        public bool Binded { get; set; }
+		
+		public void   test()
+		{
+			
+			
+		}
+
+    }
+}
